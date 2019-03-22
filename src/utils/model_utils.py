21a5e1516b23e75bc6c9ef12742c5c3fc33033f4
Siddhartha Rao Kamalakara <srk97c@gmail.com>
2019-03-23 00:59:50 +0800

    Add ramping (#31)

--- src/utils/model_utils.py
+++ src/utils/model_utils.py
@@ -4,33 +4,52 @@ from torch.nn.modules import Module
 from torch.nn.modules.utils import _pair
 import torch.nn.functional as F
 from torch.nn import init
-from src.utils.dropout import targeted_weight_dropout, targeted_unit_dropout
+from src.utils.dropout import *
 
 
 class TDConv(Module):
-    
-    def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0, dilation=1, groups=1, bias=False ):
-        super(TDConv, self).__init__()
-        self.kernel_size = _pair(kernel_size)
-        self.stride = _pair(stride)
-        self.padding = _pair(padding)
-        self.dilation = _pair(dilation)
-        self.groups = groups
-        self.floatTensor = (torch.FloatTensor if not torch.cuda.is_available() else torch.cuda.FloatTensor)
-        self.weight = nn.Parameter(self.floatTensor(out_channels, in_channels, *self.kernel_size))
-        
-        self.reset_parameters()
 
-    def reset_parameters(self):
-        init.kaiming_normal_(self.weight, mode="fan_in")
+  def __init__(self,
+               in_channels,
+               out_channels,
+               kernel_size,
+               stride=1,
+               padding=0,
+               dilation=1,
+               groups=1,
+               bias=False):
+    super(TDConv, self).__init__()
+    self.kernel_size = _pair(kernel_size)
+    self.stride = _pair(stride)
+    self.padding = _pair(padding)
+    self.dilation = _pair(dilation)
+    self.groups = groups
+    self.floatTensor = (torch.FloatTensor if not torch.cuda.is_available() else
+                        torch.cuda.FloatTensor)
+    self.weight = nn.Parameter(
+        self.floatTensor(out_channels, in_channels, *self.kernel_size))
 
-    def forward(self, x, hparams):
-        
-        if hparams.targeted_weight:
-            dropped_weights = targeted_weight_dropout(self.weight, hparams, self.training)
-        elif hparams.targeted_unit:
-            dropped_weights = targeted_unit_dropout(self.weight, hparams, self.training)
-        else:
-            dropped_weights = self.weight
+    self.reset_parameters()
 
-        return F.conv2d(x, dropped_weights, None, self.stride,self.padding, self.dilation, self.groups)
+  def reset_parameters(self):
+    init.kaiming_normal_(self.weight, mode="fan_in")
+
+  def forward(self, x, hparams):
+
+    if hparams.targeted_weight:
+      dropped_weights = targeted_weight_dropout(self.weight, hparams,
+                                                self.training)
+    elif hparams.targeted_unit:
+      dropped_weights = targeted_unit_dropout(self.weight, hparams,
+                                              self.training)
+    elif hparams.ramping_targeted_weight:
+      dropped_weights = ramping_targeted_weight_dropout(self.weight, hparams,
+                                                        self.training)
+    elif hparams.ramping_targeted_unit:
+      dropped_weights = ramping_targeted_unit_dropout(self.weight, hparams,
+                                                      self.training)
+    else:
+      dropped_weights = self.weight
+
+    return F.conv2d(x, dropped_weights, None, self.stride, self.padding,
+                    self.dilation, self.groups)
